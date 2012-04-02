2f99239db807cae2b7adb2c779a7e35ae601b58e
Edwin Boaz Soenaryo <edwinboaz@gmail.com>
2012-04-03 00:45:58 +0800

    Calculates average ILP

--- ilp.cc
+++ ilp.cc
@@ -1,5 +1,6 @@
 #include "dr_api.h"
 
+#include <stdint.h>
 #include <map>
 #include <set>
 
@@ -7,9 +8,9 @@ using namespace std;
 
 
 static void *stats_mutex; /* for multithread support */
-static int num_bb;
-static double ave_size;
-static int max_size;
+
+static int total_ni;
+static int avg_ilp; //Average ILP multiplied by 1000
 
 static void event_exit(void);
 static dr_emit_flags_t event_basic_block(void *drcontext, void *tag, instrlist_t *bb,
@@ -18,9 +19,8 @@ static dr_emit_flags_t event_basic_block(void *drcontext, void *tag, instrlist_t
 DR_EXPORT void 
 dr_init(client_id_t id)
 {
-    num_bb = 0;
-    ave_size = 0.;
-    max_size = 0;
+    total_ni = 0;
+    avg_ilp = 0.0;
     stats_mutex = dr_mutex_create();
     dr_register_bb_event(event_basic_block);
     dr_register_exit_event(event_exit);
@@ -32,14 +32,26 @@ event_exit(void)
     char msg[512];
     int len;
     len = dr_snprintf(msg, sizeof(msg)/sizeof(msg[0]),
-                      "Number of basic blocks seen: %d\n"
-                      "               Maximum size: %d instructions\n"
-                      "               Average size: %5.1f instructions\n",
-                      num_bb, max_size, ave_size);
+                      "Average instruction level parallelism: %.3f\n"
+                      "                   Instructions count: %d\n",
+                      ((double) avg_ilp / 1000.0), total_ni);
     DR_ASSERT(len > 0);
     msg[sizeof(msg)/sizeof(msg[0])-1] = '\0';
     printf("%s", msg);
+    
     dr_mutex_destroy(stats_mutex);
+}
+
+static void
+update_avg_ilp(int bb_ilp, int bb_size)
+{
+    //dr_fprintf(STDERR, "update_avg_ilp(), bb_ilp=%d, bb_size=%d\n", bb_ilp, bb_size);
+    dr_mutex_lock(stats_mutex);
+    
+    total_ni += bb_size;
+    avg_ilp += (bb_ilp - avg_ilp) / total_ni;
+    
+    dr_mutex_unlock(stats_mutex);
 }
 
 #define _MAX(x, y) (((x) > (y)) ? (x) : (y))
@@ -52,15 +64,6 @@ event_basic_block(void *drcontext, void *tag, instrlist_t *bb,
     int nc = 0;
     
     map<reg_id_t, int>  reg_nc;
-
-    /* we use fp ops so we have to save fp state */
-    byte fp_raw[512 + 16];
-    byte *fp_align = (byte *) ( (((ptr_uint_t)fp_raw) + 16) & ((ptr_uint_t)-16) );
-
-    if (translating)
-        return DR_EMIT_DEFAULT;
-
-    proc_save_fpstate(fp_align);
 
     for (instr_t* instr = instrlist_first(bb);
          instr != NULL; instr = instr_get_next(instr))
@@ -108,32 +111,23 @@ event_basic_block(void *drcontext, void *tag, instrlist_t *bb,
 	    ni++;
 	}
 
-    double ilp = 0.0;
+    int ilp = 0;
     if (nc > 0)
-        ilp = ((double) ni) / nc;
+        ilp =  (ni * 1000) / nc;
     else /* no dependencies, all can execute in parallel */
-        ilp = ni;
+        ilp = (ni * 1000);
         
-    DR_ASSERT(ilp >= 1.0);
-
-    dr_mutex_lock(stats_mutex);
-    dr_fprintf(STDERR, "ni=%d, nc=%d, ilp=%2.3f\n", ni, nc, ilp);
-    /*
-    dr_fprintf(STDERR,
-	       "Average: cur=%d, old=%8.1f, num=%d, old*num=%8.1f\n"
-	       "\told*num+cur=%8.1f, new=%8.1f\n",
-	       ni, ave_size, num_bb, ave_size*num_bb,
-	       (ave_size * num_bb) + ni,
-	       ((ave_size * num_bb) + ni) / (double) (num_bb+1));
-	       */
-
-    if (ni > max_size)
-	max_size = ni;
-    ave_size = ((ave_size * num_bb) + ni) / (double) (num_bb+1);
-    num_bb++;
-    dr_mutex_unlock(stats_mutex);
-
-    proc_restore_fpstate(fp_align);
+    DR_ASSERT(ilp >= 1000);
+    
+    //dr_fprintf(STDERR, "ni=%d, nc=%d, ilp=%d\n", ni, nc, ilp);
+    
+    instr_t* first_instr = instrlist_first(bb);
+    
+    dr_insert_clean_call(drcontext, bb, first_instr,
+                         (void*) update_avg_ilp,
+                         false, 2,
+                         OPND_CREATE_INT32(ilp),
+                         OPND_CREATE_INT32(ni));
 
     return DR_EMIT_DEFAULT;
 }
