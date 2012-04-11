b447fd373c77f1183c6454487614395f8adb3c64
Edwin Soenaryo <edwinboaz@gmail.com>
2012-04-12 00:00:35 +0800

    Added preprocessor definitions for turning optimizations on/off.

--- ilp.cc
+++ ilp.cc
@@ -1,14 +1,14 @@
 #include "dr_api.h"
 
 #include <stdint.h>
-#include <map>
-#include <set>
 #include <utility>
 #include <vector>
 
-using namespace std;
+//#define USE_CLEAN_CALLS
+//#define THREAD_SAFE_CLEAN_CALLS
+//#define FIND_DEAD_EFLAGS
 
-static void *stats_mutex; /* for multithread support */
+using namespace std;
 
 typedef struct {
     uint64_t total_ni;
@@ -16,6 +16,10 @@ typedef struct {
 } ilp_stats;
 
 static ilp_stats stats;
+
+#ifdef THREAD_SAFE_CLEAN_CALLS
+static void* stats_mutex;
+#endif
 
 static void event_exit(void);
 static dr_emit_flags_t event_basic_block(void *drcontext, void *tag,
@@ -26,7 +30,11 @@ dr_init(client_id_t id)
 {
     stats.total_ni = 0;
     stats.sum_ilp = 0;
+    
+#ifdef THREAD_SAFE_CLEAN_CALLS
     stats_mutex = dr_mutex_create();
+#endif
+    
     dr_register_bb_event(event_basic_block);
     dr_register_exit_event(event_exit);
 }
@@ -34,18 +42,12 @@ dr_init(client_id_t id)
 static void 
 event_exit(void)
 {
-    char msg[512];
-    int len;
-    double avg_ilp = stats.sum_ilp / stats.total_ni;
-    len = dr_snprintf(msg, sizeof(msg)/sizeof(msg[0]),
-                      "Average instruction level parallelism: %.3f\n"
-                      "                   Instructions count: %u\n",
-                      avg_ilp / 1000, stats.total_ni);
-    DR_ASSERT(len > 0);
-    msg[sizeof(msg)/sizeof(msg[0])-1] = '\0';
-    printf("%s", msg);
-    
+    printf("Average instruction level parallelism: %.3f\n",
+        (double) stats.sum_ilp / stats.total_ni / 1000);
+        
+#ifdef THREAD_SAFE_CLEAN_CALLS
     dr_mutex_destroy(stats_mutex);
+#endif
 }
 
 void insert_if_unique(vector<opnd_t>& vecOpnds, const opnd_t& opnd)
@@ -99,7 +101,6 @@ calculate_ilp(instrlist_t* bb, int32_t& ni, int32_t& ilp)
 {
     ni = 0;
     int nc = 0;
-    
     vector< pair<opnd_t, int> > opnd_nc;
 
     /* Look for the following types of dependencies:
@@ -111,6 +112,8 @@ calculate_ilp(instrlist_t* bb, int32_t& ni, int32_t& ilp)
     for (instr_t* instr = instrlist_first(bb);
          instr != NULL; instr = instr_get_next(instr))
     {
+        int ic = 0;
+        
         /* Process source operands */
         vector<opnd_t> unique_srcs;
         get_unique_src_opnds(unique_srcs, instr);
@@ -120,8 +123,10 @@ calculate_ilp(instrlist_t* bb, int32_t& ni, int32_t& ilp)
         {
             pair<opnd_t, int> *pMatch = get_match_by_reg(opnd_nc, *it);
             if (pMatch)
-                nc = _MAX(nc, pMatch->second);
+                ic = _MAX(ic, pMatch->second);
         }
+        
+        nc = _MAX(ic, nc);
         
         /* Process destination operands */
         vector<opnd_t> unique_dsts;
@@ -132,17 +137,12 @@ calculate_ilp(instrlist_t* bb, int32_t& ni, int32_t& ilp)
         {
             pair<opnd_t, int> *pMatch = get_match_by_reg(opnd_nc, *it);
             if (pMatch)
-            {
-                ++(pMatch->second);
-                nc = _MAX(nc, pMatch->second);
-            }
+                pMatch->second = ic + 1;
             else
-            {
                 opnd_nc.push_back(pair<opnd_t, int>(*it, 1));
-            }
         }
         
-        /* Number of instructions */
+        /* Increment the instruction count */
 	    ni++;
 	}
 	
@@ -156,6 +156,8 @@ calculate_ilp(instrlist_t* bb, int32_t& ni, int32_t& ilp)
         dr_fprintf(STDERR, "Assertion FAILED: ilp=%d\n", ilp);
         throw -1;
     }
+    
+    dr_fprintf(STDERR, "BB: size=%d, ILP=%.3f\n", ni, (double) ilp / 1000);
 }
 
 #define TESTALL(mask, var) (((mask) & (var)) == (mask))
@@ -165,53 +167,86 @@ calculate_ilp(instrlist_t* bb, int32_t& ni, int32_t& ilp)
 #define EFLAGS_DEAD(x) (TESTALL(EFLAGS_WRITE_6, (x)) \
     && !TESTANY(EFLAGS_READ_6, (x)))
 
-static dr_emit_flags_t
-event_basic_block(void *drcontext, void *tag, instrlist_t *bb,
-                  bool for_trace, bool translating)
+static bool
+find_dead_eflags_instr(instrlist_t *bb, instr_t* pos)
 {
-    int32_t num_instr = 0;
-    int32_t ilp = 0;
-    
-    calculate_ilp(bb, num_instr, ilp);
-    
-    bool eflags_save_needed = true;
-    instr_t* where = instrlist_first(bb);
     for (instr_t* ins = instrlist_first(bb);
          ins != NULL; ins = instr_get_next(ins))
     {
         unsigned int flags = instr_get_arith_flags(ins);
         if (EFLAGS_DEAD(flags))
         {
-            where = ins;
-            eflags_save_needed = false;
+            pos = ins;
+            return true;
         }
     }
-    
-    if (eflags_save_needed)
-        dr_save_arith_flags(drcontext, bb, where, SPILL_SLOT_1);
-                         
-    instrlist_meta_preinsert(bb, where,
-        LOCK(INSTR_CREATE_add(drcontext,
-        OPND_CREATE_ABSMEM((byte *)&stats.total_ni, OPSZ_4),
-        OPND_CREATE_INT32(num_instr))));
+    pos = instrlist_first(bb);
+    return false;
+}
+
+inline static void
+preinsert_add64(void* dc, instrlist_t* bb, instr_t* pos,
+                void* absmem, int32_t addend)
+{
+    instrlist_meta_preinsert(bb, pos,
+        LOCK(INSTR_CREATE_add(dc,
+        OPND_CREATE_ABSMEM((byte *)absmem, OPSZ_4),
+        OPND_CREATE_INT32(addend))));
       
-    instrlist_meta_preinsert(bb, where,
-        LOCK(INSTR_CREATE_adc(drcontext,
-        OPND_CREATE_ABSMEM((byte *)&stats.total_ni + 4, OPSZ_4),
+    instrlist_meta_preinsert(bb, pos,
+        LOCK(INSTR_CREATE_adc(dc,
+        OPND_CREATE_ABSMEM((byte *)absmem + 4, OPSZ_4),
         OPND_CREATE_INT8(0))));
-        
-    instrlist_meta_preinsert(bb, where,
-        LOCK(INSTR_CREATE_add(drcontext,
-        OPND_CREATE_ABSMEM((byte *)&stats.sum_ilp, OPSZ_4),
-        OPND_CREATE_INT32(ilp * num_instr))));
-        
-    instrlist_meta_preinsert(bb, where,
-        LOCK(INSTR_CREATE_adc(drcontext,
-        OPND_CREATE_ABSMEM((byte *)&stats.sum_ilp + 4, OPSZ_4),
-        OPND_CREATE_INT8(0))));
- 
-    if (eflags_save_needed)    
-        dr_restore_arith_flags(drcontext, bb, where, SPILL_SLOT_1);
+}
+
+static void
+update_ilp(int32_t ni, int32_t sum_offset)
+{
+#ifdef THREAD_SAFE_CLEAN_CALLS
+    dr_mutex_lock(stats_mutex);
+#endif
+
+    stats.total_ni += ni;
+    stats.sum_ilp += sum_offset;
+    
+#ifdef THREAD_SAFE_CLEAN_CALLS
+    dr_mutex_unlock(stats_mutex);
+#endif
+}
+
+static dr_emit_flags_t
+event_basic_block(void *dc, void *tag, instrlist_t *bb,
+                  bool for_trace, bool translating)
+{
+    int32_t num_instr = 0;
+    int32_t ilp = 0;
+    int32_t ilp_sum_offset = 0;
+    
+    calculate_ilp(bb, num_instr, ilp);
+    ilp_sum_offset = ilp * num_instr;
+    
+    instr_t* pos = instrlist_first(bb);
+
+#ifdef USE_CLEAN_CALLS
+    dr_insert_clean_call(dc, bb, pos, (void*) update_ilp, false, 2,
+                         OPND_CREATE_INT32(num_instr),
+                         OPND_CREATE_INT32(ilp_sum_offset));
+#else
+#ifdef FIND_DEAD_EFLAGS
+    bool dead_eflags_found = find_dead_eflags_instr(bb, pos);
+   
+    if (!dead_eflags_found)
+#endif
+        dr_save_arith_flags(dc, bb, pos, SPILL_SLOT_1);
+
+    preinsert_add64(dc, bb, pos, &stats.total_ni, num_instr);
+    preinsert_add64(dc, bb, pos, &stats.sum_ilp, ilp_sum_offset);    
+
+#ifdef FIND_DEAD_EFLAGS
+    if (!dead_eflags_found)
+#endif
+        dr_restore_arith_flags(dc, bb, pos, SPILL_SLOT_1);
+#endif
     
     return DR_EMIT_DEFAULT;
 }
